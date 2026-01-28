63d9d3258a7f49570877bf01dcdcc848f6bf69b0
Marcel Sörensen <marcel.soerensen@icloud.com>
2026-01-28 20:15:29 +0100

    feat: add Skills component and integrate into app; update WhyMe section with additional content and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,20 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { LandingPageComponent } from './landingPage/landingPage.component';
 import { WhyMeComponent } from './whyMe/whyMe.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, LandingPageComponent, WhyMeComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    LandingPageComponent,
+    WhyMeComponent,
+    SkillsComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'portfolio';
