6133400a2d2a3c1ae8401dabb0b306c9b57eefb9
Marcel Sörensen <marcel.soerensen@icloud.com>
2026-02-11 00:10:22 +0100

    feat: update legal notice section with detailed content and improve footer link for better navigation

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
 import { ProfileLogoComponent } from '../profileLogo/profileLogo.component';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule, RouterLink, ProfileLogoComponent],
+  imports: [CommonModule, ProfileLogoComponent],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
