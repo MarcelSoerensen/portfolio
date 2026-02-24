04243aebd03a8bf398c432e9024fcb1d37ec52da
Marcel Sörensen <marcel.soerensen@icloud.com>
2026-02-24 19:34:20 +0100

    feat: update contact form styling and functionality; enhance localization for 'Why Me' section; implement mail sending functionality

--- src/app/contactMe/contactForm/contactForm.component.ts
+++ src/app/contactMe/contactForm/contactForm.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject} from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { TranslocoModule } from '@jsverse/transloco';
+
 
 @Component({
   selector: 'app-contact-form',
@@ -10,6 +12,9 @@ import { TranslocoModule } from '@jsverse/transloco';
   styleUrls: ['./contactForm.component.scss']
 })
 export class ContactFormComponent {
+
+  http = inject(HttpClient);
+
   contactData = {
     name: '',
     email: '',
@@ -19,14 +24,38 @@ export class ContactFormComponent {
 
   privacyError = false;
 
+  mailTest = false;
+
+  post = {
+    endPoint: 'https://marcel-soerensen.com/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      responseType: 'text' as const,
+    },
+  };
+
   onSubmit(ngForm: NgForm) {
     if (!this.contactData.privacyChecked) {
       this.privacyError = true;
-    } else {
-      this.privacyError = false;
+      return;
     }
-    if (ngForm.valid && this.contactData.privacyChecked) {
-      console.log(this.contactData);
+    this.privacyError = false;
+    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+      this.http.post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+            ngForm.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+      ngForm.resetForm();
     }
   }
 }
